9651bdbb8e6661f6ad4cd5c204cbcd9d4ae1c29d
Gilles <gmantel@xebia.fr>
2014-11-24 00:25:02 +0100

    adding comments

--- gettingAndCleaningData/run_analysis.R
+++ gettingAndCleaningData/run_analysis.R
@@ -1,19 +1,28 @@
-
+# reading feature names and trimming numeric headers
 features <- scan("features.txt",sep = "\n",what=character())
 features <- sub("^.*[0-9] ","",features)
+# construct a vector with variables to retain
 extractVariables <- grep("std()|mean()",features)
+# getting subject data
 subjecttrain <- read.table("train/subject_train.txt", col.names="subjectID")
 subjecttest <- read.table("test/subject_test.txt", col.names="subjectID")
+# getting observation data
 trainingset <- read.table("train/X_train.txt",col.names=features)
 testset <- read.table("test/X_test.txt",col.names=features)
+# getting activity data
 actlabels <- read.table("activity_labels.txt",col.names=c("activityID","activityName"))
 activitiestest <- read.table("test/y_test.txt",col.names="activityID")
 activitiestrain <- read.table("train/y_train.txt",col.names="activityID")
+# binding the data together while selecting only the proper variables
 essai <- rbind(cbind(subjecttrain,activitiestrain,trainingset[,extractVariables]),cbind(subjecttest,activitiestest,testset[,extractVariables]))
+# adding the activity names by merging
 result <- merge(actlabels,essai)
-write.table(essai,"result.txt",row.name=FALSE)
+# creating another variable to compute the mean by aggregating by subject and activity
 means <- aggregate(result[,3] ~ subjectID + activityID, data = result, FUN = mean)
+# looping over the retained feature to compute the mean
 for (i in 4:ncol(result)){
   means[,i] <- aggregate( result[,i] ~ subjectID + activityID, data = result, FUN = mean )[,3]
 }
+# setting the column names properly
 colnames(means)[4:82]<-features[extractVariables]
+write.table(means,"result.txt",row.name=FALSE)
